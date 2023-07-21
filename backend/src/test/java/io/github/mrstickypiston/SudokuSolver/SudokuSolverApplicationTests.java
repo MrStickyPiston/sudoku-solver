2ee458253a7304d947fbcdd143174c7724213d8a
MrStickyPiston <mr.sticky.piston@gmail.com>
2023-07-21 11:55:03 +0000

    Backend progress

---
+++ backend/src/test/java/io/github/mrstickypiston/SudokuSolver/SudokuSolverApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mrstickypiston.SudokuSolver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SudokuSolverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
